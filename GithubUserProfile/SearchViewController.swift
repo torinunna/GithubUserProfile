db86cc34ca3438ae4cef3c8f2e893c3a2c0b897e
torinunna <torinunna9588@gmail.com>
2023-05-31 16:08:32 +0900

    add components to SearchVC

---
+++ GithubUserProfile/SearchViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SearchViewController.swift
+//  GithubUserProfile
+//
+//  Created by YUJIN KWON on 2023/05/31.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    @IBOutlet weak var thumbnail: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var loginLabel: UILabel!
+    @IBOutlet weak var followerLabel: UILabel!
+    @IBOutlet weak var followingLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
